efcf486d7240b70e4cc19430768261b0da1452f0
lsardev <devandfinancelsar@gmail.com>
2023-07-28 16:16:10 -0300

    update 1.2
    Updating the search engine

--- search_engine_py/master.py
+++ search_engine_py/master.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import *
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
@@ -7,6 +8,7 @@ from PyQt5.QtGui import QIcon
 import os
 import sys
 
+
 # main window
 class MainWindow(QMainWindow):
 
@@ -14,69 +16,191 @@ class MainWindow(QMainWindow):
 	def __init__(self, *args, **kwargs):
 		super(MainWindow, self).__init__(*args, **kwargs)
 
-		# creating a tab widget
+		#tab widget
 		self.tabs = QTabWidget()
 		self.tabs.setDocumentMode(True)
+
 		self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
 		self.tabs.currentChanged.connect(self.current_tab_changed)
+
 		self.tabs.setTabsClosable(True)
 		self.tabs.tabCloseRequested.connect(self.close_current_tab)
+		self.tabs.setMovable(True)
+		self.tabs.setIconSize(QtCore.QSize(220,250))
+
 		self.setCentralWidget(self.tabs)
 
-		# creating a status bar
+		#status bar
 		self.status = QStatusBar()
 		self.setStatusBar(self.status)
-		navtb = QToolBar("Navigation")
-		self.addToolBar(navtb)
+
+		#shotcurt
+
+		#principals
+		self.home_shortcut = QShortcut(QKeySequence("Alt+Home"), self)
+		self.home_shortcut.activated.connect(self.navigate_home)
+
+		self.back_shortcut = QShortcut(QKeySequence("Shift+Home"), self)
+		self.back_shortcut.activated.connect(lambda: self.tabs.currentWidget().back())
+
+		self.next_shortcut = QShortcut(QKeySequence("Ctrl+Home"), self)
+		self.next_shortcut.activated.connect(lambda: self.tabs.currentWidget().forward())
+
+		self.reload_shortcut = QShortcut(QKeySequence("F5"), self)
+		self.reload_shortcut.activated.connect(lambda: self.tabs.currentWidget().reload())
+
+		self.newtab_shortcut = QShortcut(QKeySequence("Ctrl+T"), self)
+		self.newtab_shortcut.activated.connect(self.add_new_tab)
+
+		self.newtab_shortcut = QShortcut(QKeySequence("Ctrl+F7"), self)
+		self.newtab_shortcut.activated.connect(self.close_current_tab)
+
+
+		#seconds
+		self.yt_shortcut = QShortcut(QKeySequence("Ctrl+Y"), self)
+		self.yt_shortcut.activated.connect(self.navigate_youtube)
+
+		self.gm_shortcut = QShortcut(QKeySequence("Ctrl+G"), self)
+		self.gm_shortcut.activated.connect(self.navigate_gmail)
+
+		self.gh_shortcut = QShortcut(QKeySequence("Ctrl+H"), self)
+		self.gh_shortcut.activated.connect(self.navigate_github)
+
+		self.stk_shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
+		self.stk_shortcut.activated.connect(self.navigate_stackoverflow)
+
+		self.insta_shortcut = QShortcut(QKeySequence("Ctrl+I"), self)
+		self.insta_shortcut.activated.connect(self.navigate_instagram)
+
+		self.face_shortcut = QShortcut(QKeySequence("Ctrl+F"), self)
+		self.face_shortcut.activated.connect(self.navigate_facebook)
+
+		self.tw_shortcut = QShortcut(QKeySequence("Ctrl+X"), self)
+		self.tw_shortcut.activated.connect(self.navigate_twitter)
+
+		#creating navigation
+		navb = QToolBar("Navigation")
+		navb.setFixedWidth(1620)
+		self.addToolBar(navb)
 
 		# creating back action
 		back_btn = QAction(QIcon("icons/back.png") ,"Back", self)
-		back_btn.setStatusTip("Back to previous page")
-		back_btn.text("Back to previous page")
+		back_btn.setStatusTip("Back to previous page (Shift + Home)")
 		back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
-		navtb.addAction(back_btn)
+		navb.addAction(back_btn)
 
 		# similarly adding next button
 		next_btn = QAction(QIcon("icons/forward.png") ,"Forward", self)
-		next_btn.setStatusTip("Forward to next page")
+		next_btn.setStatusTip("Forward to next page (Ctrl + Home)")
 		next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
-		navtb.addAction(next_btn)
+		navb.addAction(next_btn)
 
 		# similarly adding reload button
 		reload_btn = QAction(QIcon("icons/reload.png"),"Reload", self)
-		reload_btn.setStatusTip("Reload page")
+		reload_btn.setStatusTip("Reload page (F5)")
 		reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
-		navtb.addAction(reload_btn)
+		navb.addAction(reload_btn)
 
 		# creating home action
 		home_btn = QAction(QIcon("icons/home.png") ,"Home", self)
-		home_btn.setStatusTip("Go home")
+		home_btn.setStatusTip("Go home (Alt + Home)")
 
 		# adding action to home button
 		home_btn.triggered.connect(self.navigate_home)
-		navtb.addAction(home_btn)
+		navb.addAction(home_btn)
 
 		# adding a separator
-		navtb.addSeparator()
+		navb.addSeparator()
 
 		# creating a line edit widget for URL
 		self.urlbar = QLineEdit()
 		self.urlbar.returnPressed.connect(self.navigate_to_url)
-		navtb.addWidget(self.urlbar)
+		navb.addWidget(self.urlbar)
 
 		# similarly adding stop action
-		stop_btn = QAction("Stop", self)
+		stop_btn = QAction(QIcon("icons/stop.png"),"Stop", self)
 		stop_btn.setStatusTip("Stop loading current page")
 		stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
-		navtb.addAction(stop_btn)
+		navb.addAction(stop_btn)
+
+		navb2 = QToolBar("Navigation 2")
+		navb2.setFixedWidth(300)
+		self.addToolBar(navb2)
+
+		# creating youtube
+		yt_btn = QAction(QIcon("icons/youtube.png") ,"Youtube", self)
+		yt_btn.setStatusTip("Go to Youtube")
+		yt_btn.triggered.connect(self.navigate_youtube)
+		navb2.addAction(yt_btn)
+
+
+		#creating gmail
+		gm_btn = QAction(QIcon("icons/gmail.png") ,"Gmail", self)
+		gm_btn.setStatusTip("Go to Gmail")
+		gm_btn.triggered.connect(self.navigate_gmail)
+		navb2.addAction(gm_btn)
+
+		# creating github
+		gith_btn = QAction(QIcon("icons/github.png") ,"Github", self)
+		gith_btn.setStatusTip("Go to GitHub")
+		gith_btn.triggered.connect(self.navigate_github)
+		navb2.addAction(gith_btn)
+
+		#creating stackoverflow
+		stk_btn = QAction(QIcon("icons/stackoverflow.png") ,"Stack Overflow", self)
+		stk_btn.setStatusTip("Go to Stack Overflow")
+		stk_btn.triggered.connect(self.navigate_stackoverflow)
+		navb2.addAction(stk_btn)
+
+		#creating instagram
+		insta_btn = QAction(QIcon("icons/instagram.png") ,"Instagram", self)
+		insta_btn.setStatusTip("Go to Instagram")
+		insta_btn.triggered.connect(self.navigate_instagram)
+		navb2.addAction(insta_btn)
+
+		#creating facebook
+		face_btn = QAction(QIcon("icons/facebook.png") ,"Facebook", self)
+		face_btn.setStatusTip("Go to Facebook")
+		face_btn.triggered.connect(self.navigate_facebook)
+		navb2.addAction(face_btn)
+
+		#creating twitter
+		tw_btn = QAction(QIcon("icons/twitter.png") ,"Twitter", self)
+		tw_btn.setStatusTip("Go to Twitter")
+		tw_btn.triggered.connect(self.navigate_twitter)
+		navb2.addAction(tw_btn)
 
 		# creating first tab
 		self.add_new_tab(QUrl('http://www.google.com'), 'Homepage')
 
-	
 		self.showMaximized()
 		self.setWindowTitle("new window")
-		
+
+	#navigates
+	# action to go to home
+	def navigate_home(self):
+		self.tabs.currentWidget().setUrl(QUrl("http://www.google.com"))
+
+	def navigate_youtube(self):
+		self.tabs.currentWidget().setUrl(QUrl("http://www.youtube.com"))
+
+	def navigate_gmail(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://www.google.com/gmail/about/"))
+
+	def navigate_github(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://github.com/"))
+
+	def navigate_stackoverflow(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://stackoverflow.com/"))
+
+	def navigate_instagram(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://www.instagram.com/"))
+	
+	def navigate_facebook(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://www.facebook.com/"))
+
+	def navigate_twitter(self):
+		self.tabs.currentWidget().setUrl(QUrl("https://twitter.com/"))
         
 
 	# method for adding new tab
@@ -134,12 +258,6 @@ class MainWindow(QMainWindow):
 		# get the page title
 		title = self.tabs.currentWidget().page().title()
 		self.setWindowTitle("% s - tab" % title)
-
-	# action to go to home
-	def navigate_home(self):
-
-		# go to google
-		self.tabs.currentWidget().setUrl(QUrl("http://www.google.com"))
 
 	# method for navigate to url
 	def navigate_to_url(self):
